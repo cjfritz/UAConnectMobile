c070709a9f9c4bd2d12a714df63b471fe7664d68
Caleb Fritz <caleb.fritz@jbhunt.com>
2018-10-14 12:21:13 -0500

    Initial master setup

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Header, Title } from 'native-base';
+
+const App = () => (
+  <Container>
+    <Header>
+      <Title>Hello, World!</Title>
+    </Header>
+  </Container>
+);
+
+export default App;
