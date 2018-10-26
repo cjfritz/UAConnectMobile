ac782adde25aef8a03a6c11dea3beca6a6befc19
Caleb Fritz <caleb.fritz@jbhunt.com>
2018-10-26 18:40:59 -0500

    added login and home sceens, loading indicator and toast, styles too

---
+++ src/components/common/LoadingModal/LoadingModal.style.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    bottom: 0,
+    justifyContent: 'center',
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+  modalStyle: {
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+  spinner: {
+    color: 'red',
+  },
+  text: {
+    color: 'white',
+  },
+});
