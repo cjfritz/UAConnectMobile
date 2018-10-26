a69088c6a6fe5c27ce02e633c94fec29f8fabac0
Caleb Fritz <caleb.fritz@jbhunt.com>
2018-10-26 18:40:05 -0500

    added auth action creators, initial tests, and types

---
+++ src/actions/types.js
@@ -0,0 +1,8 @@
+export const EMAIL_CHANGED = 'email_changed';
+export const PASSWORD_CHANGED = 'password_changed';
+export const LOGIN_USER_SUCCESS = 'login_user_success';
+export const LOGIN_USER_FAIL = 'login_user_fail';
+export const LOGIN_USER = 'login_user';
+export const LOGOUT_USER = 'logout_user';
+export const LOGOUT_USER_SUCCESS = 'logout_user_success';
+export const LOGOUT_USER_FAIL = 'logout_user_fail';
