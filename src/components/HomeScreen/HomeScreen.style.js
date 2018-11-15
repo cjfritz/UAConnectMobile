eb78bb0054649ef2e40d414e457c35d300bed7ce
Anjan Poudel <ap378@uark.edu>
2018-11-15 15:07:05 -0600

    Added base homescreen functionality

---
+++ src/components/HomeScreen/HomeScreen.style.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  Buttons: {
+    backgroundColor: "#7b285f",
+    flex: 7
+  },
+  Container: {
+    alignContent: 'center',
+    backgroundColor: '#67141A',
+    flex: 1,
+  },
+  Header: {
+    flex: 2,
+  },
+  News: {
+    backgroundColor: 'white',
+    flex: 1
+  },
+  uaconnect: {
+    color: 'white', 
+    fontSize: 50,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  }
+});
