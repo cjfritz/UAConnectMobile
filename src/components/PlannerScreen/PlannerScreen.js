6a15440830e659a319eb914036222c8d1b5b6344
Caleb Fritz <caleb.fritz@jbhunt.com>
2018-11-25 12:31:03 -0600

    added planner screen component

---
+++ src/components/PlannerScreen/PlannerScreen.js
@@ -0,0 +1,152 @@
+/* eslint-disable no-restricted-globals */
+import React, { Component } from 'react';
+import { FlatList, View } from 'react-native';
+import { connect } from 'react-redux';
+import { map, reduce } from 'lodash';
+import {
+  Container, Content, Text, Button, Grid, Col, Row,
+} from 'native-base';
+import CourseCard from '../common/CourseCard/CourseCard';
+import { plannerFetch } from '../../actions/PlannerActions';
+import { LoadingModal } from '../common/LoadingModal/LoadingModal';
+import styles from './PlannerScreen.style';
+
+export class PlannerScreen extends Component {
+  static navigationOptions = ({ navigation }) => (
+    {
+      title: 'Course Planner',
+      headerRight: (() => (
+        <Text style={ styles.headerButtonText } onPress={ () => navigation.navigate('PlannerCreateComponent') }>
+          Add Course
+        </Text>
+      ))(),
+      headerStyle: {
+        backgroundColor: 'darkred',
+      },
+      headerTitleStyle: {
+        color: 'white',
+        fontSize: 20,
+      },
+    }
+  );
+
+  componentWillMount() {
+    const { firebasePlannerFetch } = this.props;
+    firebasePlannerFetch();
+  }
+
+  renderItem = item => {
+    const { navigation } = this.props;
+    return (
+      <CourseCard navigation={ navigation } courseItem={ item } />
+    );
+  };
+
+  onReload = () => {
+    const { firebasePlannerFetch } = this.props;
+    firebasePlannerFetch();
+  };
+
+  renderList = () => {
+    const { planner, loading } = this.props;
+    if (!loading && planner.length !== 0) {
+      return (
+        <FlatList
+          data={ planner }
+          renderItem={ ({ item }) => this.renderItem(item) }
+          keyExtractor={ item => item.uid }
+        />
+      );
+    } else if (!loading) {
+      return (
+        <View style={ styles.reloadView }>
+          <Text style={ styles.reloadText }>No Courses Found</Text>
+          <Button style={ styles.reloadButton } onPress={ this.onReload }>
+            <Text style={ styles.titleText }>
+              Reload
+            </Text>
+          </Button>
+        </View>
+      );
+    }
+    return null;
+  };
+
+  getAverageGPA = () => {
+    const { planner } = this.props;
+    let gradeCounter = 0;
+    const gradeTotal = reduce(planner, (sum, item) => {
+      const grade = Number(item.grade);
+      if (!isNaN(grade) && item.grade !== '') {
+        gradeCounter += 1;
+        return sum + grade;
+      } else {
+        return sum;
+      }
+    }, 0);
+    return (gradeTotal / gradeCounter).toFixed(2);
+  };
+
+  getTotalUnits = () => {
+    const { planner } = this.props;
+    const unitTotal = reduce(planner, (sum, item) => {
+      const units = Number(item.units);
+      if (!isNaN(units) && item.units !== '') {
+        return sum + units;
+      } else {
+        return sum;
+      }
+    }, 0);
+    return unitTotal;
+  };
+
+  render() {
+    const { loading } = this.props;
+    const averageGPA = this.getAverageGPA();
+    const totalUnits = this.getTotalUnits();
+    return (
+      <Container style={ styles.container }>
+        <LoadingModal visible={ loading } loadingLabel='Loading...' />
+        <View style={ styles.infoHeader }>
+          <Grid>
+            <Col style={ styles.col }>
+              <Row style={ styles.row }>
+                <Text style={ styles.titleText }>Average GPA</Text>
+              </Row>
+              <Row style={ styles.row }>
+                <Text style={ styles.bodyText }>{ isNaN(averageGPA) ? 'N/A' : averageGPA }</Text>
+              </Row>
+            </Col>
+            <Col style={ styles.col }>
+              <Row style={ styles.row }>
+                <Text style={ styles.titleText }>Total Units</Text>
+              </Row>
+              <Row style={ styles.row }>
+                <Text style={ styles.bodyText }>{ isNaN(totalUnits) ? 'N/A' : totalUnits }</Text>
+              </Row>
+            </Col>
+          </Grid>
+        </View>
+        <Content>
+          { this.renderList() }
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => (
+  {
+    firebasePlannerFetch: () => dispatch(plannerFetch()),
+  }
+);
+
+const mapStateToProps = state => {
+  const { loading, courses } = state.planner;
+  const planner = map(courses, (val, uid) => (
+    { ...val, uid }
+  ));
+  return { planner, loading };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PlannerScreen);
---
+++ src/components/PlannerScreen/PlannerScreen.style.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  bodyText: {
+    color: 'white',
+    fontSize: 18,
+  },
+  col: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    justifyContent: 'center',
+  },
+  headerButtonText: {
+    color: 'white',
+    right: '20%',
+  },
+  infoHeader: {
+    backgroundColor: 'darkred',
+    height: '10%',
+    width: '100%',
+  },
+  reloadButton: {
+    alignSelf: 'center',
+    backgroundColor: 'darkred',
+  },
+  reloadText: {
+    color: 'black',
+    fontSize: 18,
+  },
+  reloadView: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  row: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleText: {
+    color: 'white',
+    fontSize: 20,
+  },
+});
