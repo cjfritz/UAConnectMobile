d2fea039c51bcf20ddefc2e44854533596f843b7
Caleb Fritz <caleb.fritz@jbhunt.com>
2018-11-25 12:31:39 -0600

    added form picker component

---
+++ src/components/common/FormPicker/FormPicker.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  icon: {
+    width: '100%',
+  },
+  pickerHeader: {
+    backgroundColor: 'darkred',
+    flexDirection: 'row',
+  },
+  titleText: {
+    color: 'white',
+    fontSize: 20,
+  },
+});
