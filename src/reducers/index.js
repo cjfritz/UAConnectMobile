60b38878ff28b663b836242e5abd502648d3f31b
Caleb Fritz <caleb.fritz@jbhunt.com>
2018-11-30 23:47:34 -0600

    new profile state added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
 import PlannerReducer from './PlannerReducer';
 import PlannerFormReducer from './PlannerFormReducer';
+import ProfileReducer from './ProfileReducer';
 // import more reducers here to be comibined into the master reducer
 
 export default combineReducers({
   auth: AuthReducer,
   planner: PlannerReducer,
   plannerForm: PlannerFormReducer,
+  profile: ProfileReducer,
   // add more state here
 });
